1adbd3ef3e13c6c3195a3a7ecc92abd3bd5c4f82
hergot <milan.hradil@email.cz>
2014-10-08 14:01:15 +0200

    Add initial commit of helo command

---
+++ commands/helo.js
@@ -0,0 +1,5 @@
+var CMD_SEPARATOR = "\r\n";
+
+module.exports = function(commandLine, output, cb, socket) {
+    output('Welcome to mailproxy' + CMD_SEPARATOR);
+}
--- server.js
+++ server.js
@@ -3,13 +3,51 @@
 var net = require('net');
 var argv = require('minimist')(process.argv.slice(2));
 var port = argv['p'] || 5000;
+var CMD_SEPARATOR = "\r\n";
+var SUPPORTED_COMMANDS = ['HELO', 'EHLO', 'MAIL', 'RCPT', 'DATA', 'RSET', 'NOOP', 'QUIT', 'VRFY'];
 
 process.stdout.write('Starting at port ' + port + "\n");
 
 net.createServer(function(socket) {
-    socket.write('Welcome');
+    var queue = [];
+    socket.write('Welcome to mailproxy' + "\n");
     socket.remoteAddress;
     socket.remotePort;
-    socket.on('data', function() {});
-    socket.on('end', function() {});
+    socket.on('data', function(data) {
+        if (queue.length > 0) {
+            var processor = queue.shift();
+            processor(data, function(text) {
+                    socket.write(text);
+                }, function(cb) {
+                    if (cb) {
+                        queue.push(cb);
+                    }
+               }, socket);
+        } else {
+            dispatch(data, function(text) {
+                    socket.write(text);
+                }, function(cb) {
+                    if (cb) {
+                        queue.push(cb);
+                    }
+                }, socket);
+        }
+    });
+    socket.on('end', function() {
+        process.stdout.write('Socket closed' + "\n");
+    });
 }).listen(port);
+
+function dispatch(data, output, cb, socket) {
+//    process.stdout.write('>' + String(data).replace(CMD_SEPARATOR, '') + '<' + "\n");
+    var commandLine = (String(data).replace(CMD_SEPARATOR, ''));
+    var command = commandLine.split(' ')[0];
+    if (SUPPORTED_COMMANDS.indexOf(command.toUpperCase()) === -1) {
+    // handle error - not supported command
+        output('500 Syntax error, command unrecognised' + CMD_SEPARATOR);
+        return;
+    }
+    var command = require(__dirname + '/commands/' + command.toLowerCase() + '.js');
+    command(commandLine, output, cb, socket);
+};
+
